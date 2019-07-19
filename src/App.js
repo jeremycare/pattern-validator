d690dcf85b462cf031ba13327d0185015e3039d0
Jeremy Care <jeremy@wisepops.com>
2019-07-20 00:05:42 +0200

    feat: init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>POC: pattern-validator input check by Jeremy Care</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
